71327219898e204e5fc2063a4d2a957f77619419
Viacheslav <markvyach83@gmail.com>
2021-04-10 20:03:50 +0300

    Initial Commit

---
+++ TechnicalTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TechnicalTask
+//
+//  Created by Viacheslav Markov on 10.04.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
